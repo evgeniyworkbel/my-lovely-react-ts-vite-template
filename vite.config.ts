24a7fe6c4798039e1fdd0f2fc6be5f7b46f6cecb
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-10 21:13:32 +0300

    build: set import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set base url to deploy to Github Pages (see https://vite.dev/guide/static-deploy.html#github-pages)
+  // base: "/new-app/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
 });
